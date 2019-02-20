9b84152188840e09915da086c5b524ddc33bdb49
yanglhan <836564993@qq.com>
2019-02-20 17:10:32 +0800

    Initial Commit

---
+++ YYPlayView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YYPlayView
+//
+//  Created by etz on 2019/2/20.
+//  Copyright © 2019年 alibaba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YYPlayView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YYPlayView
+//
+//  Created by etz on 2019/2/20.
+//  Copyright © 2019年 alibaba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
